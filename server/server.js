19e03d570a6d99934411eee62686c9fb0826a1bd
Daniel Goye <danielgoye92@gmail.com>
2024-01-29 13:53:46 +0300

    Reorganise project into client and server sections

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const dev = process.env.NODE_ENV !== "production";
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const next = require("next");
-const app = next({ dev });
-const handle = app.getRequestHandler();
 const { dbConnect } = require("./config/databaseConfig");
 const path = require("path");
 const server = express();
@@ -33,12 +30,6 @@ server.get("/api/warmup", (req, res) => {
 server.use("/api/auth", require("./routes/authRoutes"));
 server.use("/api/users", require("./routes/userRoutes"));
 server.use("/api/posts", require("./routes/postsRoutes"));
-
-app.prepare().then(() => {
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
-});
 
 server.listen(PORT, () => {
   console.log(`Server at port ${PORT} is running`);
